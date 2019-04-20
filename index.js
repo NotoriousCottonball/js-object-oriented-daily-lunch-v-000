9eabe8072fa1d048f86dd7251aa04d7c53e42136
George  Pianka <gpianka1@gmail.com>
2019-04-20 14:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,26 @@ class Neighborhood {
     let meals = this.customers().map(customer => customer.meals());
     return [...new Set(meals[0])];
     }
-    
-    
+}
+
+class Meal {
+    constructor(title, price) {
+        this.id = ++mealId;
+        this.title = title;
+        this.price = price;
+        store.meals.push(this);
+    }
+    deliveries(){
+      return store.deliveries.filter(delivery => {
+        return delivery.mealId === this.id;
+      });
+    }  
+    customers() {
+      return this.deliveries().map(delivery => {
+        return store.customers.find(customer => 
+        customer.id === delivery.customerId)});
+    }  
+    static byPrice() {
+      return store.meals.sort(function(a, b) {return b.price - a.price});
+    }
 }
